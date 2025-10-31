0a51fe2f183fb8f80e07a98a3d1a517b60afa724
aishsanal <aiswaryasanal9@gmail.com>
2025-10-31 18:57:27 +0530

    Learnt more on rendering templates and layouts

---
+++ hello-world/pkg/handlers/handler.go
@@ -0,0 +1,103 @@
+package handlers
+
+import (
+	"fmt"
+	"html/template"
+	"log"
+	"net/http"
+	"path/filepath"
+)
+
+func Home(w http.ResponseWriter, r *http.Request) {
+	renderTemplate(w, "home.page.tmpl")
+}
+
+func About(w http.ResponseWriter, r *http.Request) {
+	renderTemplate(w, "about.page.tmpl")
+}
+
+func renderTemplate(w http.ResponseWriter, t string) {
+	cache, err := createTemplateCache()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	template := cache[t]
+	err = template.Execute(w, nil)
+	if err != nil {
+		log.Fatal(err)
+	}
+}
+
+func createTemplateCache() (map[string]*template.Template, error) {
+	cache := map[string]*template.Template{}
+
+	pagesPath, err := filepath.Glob("../../templates/*.page.tmpl")
+	if err != nil {
+		return cache, err
+	}
+
+	for _, path := range pagesPath {
+		name := filepath.Base(path)
+		templ, err := template.New(name).ParseFiles(path)
+		if err != nil {
+			return cache, err
+		}
+
+		layoutPath, err := filepath.Glob("../../templates/*.layout.tmpl")
+		if err != nil {
+			return cache, err
+		}
+
+		if len(layoutPath) > 0 {
+			templ, err = templ.ParseGlob(layoutPath[0])
+			if err != nil {
+				return cache, err
+			}
+		}
+		cache[name] = templ
+	}
+	return cache, nil
+}
+
+// The below functions were written first and the above ones are the refined versions
+var tempCache = make(map[string]*template.Template)
+
+func renderTemplateOld(w http.ResponseWriter, t string) {
+	var tmpl *template.Template
+	var err error
+	_, inMap := tempCache[t]
+	if !inMap {
+		log.Println("Loading template from disk")
+		tmpl, err = loadTemplateOld(t)
+		if err != nil {
+			log.Println("Error loading template from disk")
+			return
+		}
+		tempCache[t] = tmpl
+	} else {
+		log.Println("Loading template from template cache")
+		tmpl = tempCache[t]
+	}
+	err = tmpl.Execute(w, nil)
+	if err != nil {
+		log.Println("Error rendering template ", t)
+	}
+}
+
+func loadTemplateOld(t string) (*template.Template, error) {
+	templatesToLoad := []string{
+		fmt.Sprintf("../../templates/%s", t),
+		"../../templates/base.layout.tmpl",
+	}
+	tmpl, err := template.ParseFiles(templatesToLoad...)
+	return tmpl, err
+}
+
+// func renderTemplate(w http.ResponseWriter, tmpl string) {
+// 	parsedTemplate, _ := template.ParseFiles("../../templates/"+tmpl, "../../templates/base.layout.tmpl")
+// 	err := parsedTemplate.Execute(w, nil)
+// 	if err != nil {
+// 		fmt.Println("error while rendering template:", err)
+// 	}
+// }
